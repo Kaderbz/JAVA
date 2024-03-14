8ebf64baccaccf41f41385a5d11e2146c84706f0
kaderbz <bouzouita.abdelkader@gmail.com>
2024-03-14 11:57:25 +0100

    Hopper commit

---
+++ Week3/Practice/hopper/src/main/java/com/javastack/spring/hopper/HopperApplication.java
@@ -0,0 +1,13 @@
+package com.javastack.spring.hopper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopperApplication.class, args);
+	}
+
+}
