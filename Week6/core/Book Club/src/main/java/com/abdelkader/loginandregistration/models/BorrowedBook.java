7ef9fdb9386251c72874cb35b23b8d553f2cb6b2
kaderbz <bouzouita.abdelkader@gmail.com>
2024-04-07 23:07:02 +0100

    Week 6 commit

---
+++ Week6/core/Book Club/src/main/java/com/abdelkader/loginandregistration/models/BorrowedBook.java
@@ -0,0 +1,11 @@
+package com.abdelkader.loginandregistration.models;
+
+//import javax.persistence.Entity;
+//import javax.persistence.Table;
+
+//@Entity
+//@Table(name="borrowedBooks")
+public class BorrowedBook {
+
+	
+}
